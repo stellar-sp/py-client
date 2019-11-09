4e95352e7fdfbbaad7d48bc5da1f316740182f9c
mahdi <mallakimahdi@gmail.com>
2019-11-09 11:22:00 +0330

    m

--- client_getter_role.py
+++ client_getter_role.py
@@ -13,6 +13,7 @@ smart_account = horizon.account(SMART_ACCOUNT_ADDRESS)
 
 hash_file = upload_file_to_ipfs("./sample_get_vote_input_file.json")
 worker_1_peer_address = base64.b64decode(smart_account['data']['worker_1_peer_address']).decode()
-result = requests.get(worker_1_peer_address + '/api/smart_program/' + SMART_ACCOUNT_ADDRESS, params={"input_file_hash": hash_file})
+result = requests.get("http://" + worker_1_peer_address + '/api/smart_program/' + SMART_ACCOUNT_ADDRESS,
+                      params={"input_file_hash": hash_file})
 
 print(result.text)
